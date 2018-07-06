5d088d0b65784cd7e58f81aedb82ea7e611bbd91
Paulo Renato <31320462+renatopaulobs@users.noreply.github.com>
2018-07-06 10:49:35 -0300

    Insert copy file. Notes.node

---
+++ copy.dependencies.js
@@ -0,0 +1,19 @@
+const fs = require('fs-extra');
+const packages = JSON.parse(fs.readFileSync('./package.json', 'utf-8'));
+const nodeModulesPath = './node_modules';
+const dependenciesPath = './dependencies';
+if (packages && packages.dependencies) {
+    let installedDependencies = [];
+    if (fs.existsSync(nodeModulesPath)) {
+        installedDependencies = fs.readdirSync(nodeModulesPath);
+        fs.emptyDirSync(dependenciesPath);
+        for (var dep in packages.dependencies) {
+            if (installedDependencies.find(dir => dir === dep)) {
+                fs.copySync(`${nodeModulesPath}/${dep}`, `${dependenciesPath}/${dep}`);
+                console.log(`Copied dependency: ${dep}`);
+            }
+        }
+    } else {
+        console.log('Install dependencies first');
+    }
+}
