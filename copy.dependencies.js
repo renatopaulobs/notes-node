79e26fbc548c20ec185f1c281dd072c03f672652
Paulo Renato <31320462+renatopaulobs@users.noreply.github.com>
2018-07-11 16:14:36 -0300

    Files

--- copy.dependencies.js
+++ copy.dependencies.js
@@ -1,17 +1,22 @@
 const fs = require('fs-extra');
+const licensesList = require('./manage.dependencies.js');
+const licenses = licensesList();
 const packages = JSON.parse(fs.readFileSync('./package.json', 'utf-8'));
 const nodeModulesPath = './node_modules';
 const dependenciesPath = './dependencies';
-const licensesList = require('./dependecies-manager.js');
-const licenses = licensesList();
-const aboutFileName = 'About_SC.txt';
+const aboutFileName = 'About_SC_SIDI.txt';
+const pendingFileName = 'Pending_About_SC_SIDI.txt';
 const solutionName = 'Solution - Microservice SC';
 const versionName = 'Version 00.01.00';
-var typeLicense = 0;
+
+const noInfo = '\n****Following Packages May Not Have License Information****\n\n';
+const headerInfo = `The following software may be included in this product:\n\n`;
+const noCopyrightInfo = '****Without Copyright Information****';
 
 //fs.writeFileSync('licenses-data.json', JSON.stringify(licenses, undefined, 2))
 //Copy dependencies from node_modules to dependencies
 if (packages && packages.dependencies) {
+    var typeLicense = 0;
     let installedDependencies = [];
     if (fs.existsSync(nodeModulesPath)) {
         installedDependencies = fs.readdirSync(nodeModulesPath);
@@ -31,28 +36,37 @@ if (packages && packages.dependencies) {
     }else {
         console.log('Install dependencies first');
     }
+    //Setting About file  
+    if (fs.existsSync(dependenciesPath)) {
+        let groupDependencies = [];
+        groupDependencies = fs.readdirSync(dependenciesPath);
+        fs.writeFileSync(aboutFileName, `${solutionName}\n${versionName}\n\n`)
+        fs.writeFileSync(pendingFileName, `Pending Information Packages:\n\n`)
+        groupDependencies.forEach((name) => {
+            if(name === 'No License'){
+                fs.appendFileSync(aboutFileName, noInfo)
+            }
+            let licenseBody;
+            let insideDependencies = fs.readdirSync(`${dependenciesPath}/${name}`);           
+            fs.appendFileSync(aboutFileName, headerInfo)
+            insideDependencies.forEach((dep) => {
+                for(var i=0; i<licenses.length; i++){
+                    if(dep === licenses[i].name){
+                        licenseBody = licenses[i].text;
+                        fs.appendFileSync(aboutFileName, `${licenses[i].name} v${licenses[i].version}\n${licenses[i].copyright}\n\n`);
+                        console.log(`Added dependency: ${dep}`);
+                        if(licenses[i].copyright === noCopyrightInfo || licenses[i].type === null){
+                            fs.appendFileSync(pendingFileName, `${licenses[i].name} v${licenses[i].version}\n`); 
+                        }
+                    }
+                }
+            });
+            fs.appendFileSync(aboutFileName, `${licenseBody}\n-----\n`)
+        });
+    }else {
+        console.log('Unable to find dependencies path')
+    }
 }
 
-//Setting About file  
-if (fs.existsSync(dependenciesPath)) {
-    let groupDependencies = [];
-    groupDependencies = fs.readdirSync(dependenciesPath);
-    fs.writeFileSync(aboutFileName, `${solutionName}\n${versionName}\n\n`)
-    groupDependencies.forEach((name) => {
-        let licenseBody;
-        let insideDependencies = fs.readdirSync(`${dependenciesPath}/${name}`)
-        fs.appendFileSync(aboutFileName, `The following software may be included in this product:\n\n`)
-        insideDependencies.forEach((dep) => {
-            for(var i=0; i<licenses.length; i++){
-                if(dep === licenses[i].name){
-                    licenseBody = licenses[i].text;
-                    fs.appendFileSync(aboutFileName, `${licenses[i].name} v${licenses[i].version}\n${licenses[i].copyright}\n\n`);
-                    console.log(`Added dependency: ${dep}`);
-                }
-            }
-        });
-        fs.appendFileSync(aboutFileName, `${licenseBody}\n-----\n\n`)
-    });
-}else {
-    console.log('Unable to find dependencies path')
-}
+
+
--- manage.dependencies.js
+++ manage.dependencies.js
@@ -2,7 +2,9 @@ const fs = require('fs-extra');
 const packages = JSON.parse(fs.readFileSync('./package.json', 'utf-8'));
 const projectPath = './';
 const licenses = [];
+
 var licenseBody = [];
+var copyright = [];
 
 //Getting License Info from nodemodule file
 const getLicenseInfo = (path) => {
@@ -15,17 +17,24 @@ const getLicenseInfo = (path) => {
   return fs.readFileSync(`${path}/${license}`).toString();
 }
 
+//Verify if string contains a word in the array
+const contains = (text, wordArray) => {
+  var exists = 0;
+  wordArray.forEach((word) => { exists = exists + text.includes(word); });
+  return (exists === 1)
+}
+
 //Getting Copyright from License Text
 const getCopyright = (path) => {
-  var copyright = [];
+  copyright = [];
   licenseBody = [];
+  var copyrightNames = ['Copyright (c)', 'Copyright (C)', 'Amazon.com, Inc. or its affiliates'];
   if(getLicenseInfo(path)) {
       var numberCopy = 0;
       var numberText = 0;
       var allText = getLicenseInfo(path).split('\n');
-
     for(var subText=0; subText<allText.length; subText++){
-      if(allText[subText].includes('Copyright')){
+      if(contains(allText[subText], copyrightNames)){
         copyright[numberCopy] = allText[subText];
         numberCopy++;    
       } else {
@@ -37,7 +46,7 @@ const getCopyright = (path) => {
   }
 }
 
-//Getting all licenses from package.json and listing
+//Getting all name licenses from package.json and their properties from node_modules
 const getLicenses = (path = projectPath) => {
   if (packages) {
     Object.keys(packages.dependencies).forEach((name) => {
@@ -45,7 +54,7 @@ const getLicenses = (path = projectPath) => {
         licenses.push({
           name: name,
           path: `${path}/node_modules/${name}`,
-          copyright: getCopyright(`${path}/node_modules/${name}`),
+          copyright: getCopyright(`${path}/node_modules/${name}`) || '****Without Copyright Information****',
           text: licenseBody.join('\n'),        
           type: packageJson.license || null,
           version: packageJson.version,
@@ -56,3 +65,4 @@ const getLicenses = (path = projectPath) => {
 }
 
 module.exports = () => getLicenses(projectPath);
+
