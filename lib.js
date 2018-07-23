98651aec4b131c7e66e72b07ee499f7da8f6cb19
Paulo Renato <31320462+renatopaulobs@users.noreply.github.com>
2018-07-23 10:00:42 -0300

    Adding Libraries

---
+++ lib.js
@@ -0,0 +1,19 @@
+'use strict';
+//----------------------------- External libs ------------------------------- 
+require('@uirouter/angularjs');
+require('angular');
+require('angular-animate');
+require('angular-aria');
+require('angular-material');
+require('angular-resource');
+
+require('angular-sanitize');
+require('angular-translate');
+require('angular-translate-handler-log');
+require('angular-translate-loader-static-files');
+
+require('ng-quill');
+require('ng-file-upload');
+//----------------------------- External styles ------------------------------- 
+require('./app/shared/opensource/quill.snow.css');
+require('./node_modules/angular-material/angular-material.css');
